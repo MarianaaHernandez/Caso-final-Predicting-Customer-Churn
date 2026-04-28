3afc693a5d3d4e2c1b4c6eb8afc2d81888699d0a
Sofia Acuña <sofia.acunac@javeriana.edu.co>
2026-04-27 21:58:04 -0500

    Código

---
+++ QWE_Churn.R
@@ -0,0 +1,12 @@
+# Caso: Predicción y Prevención de Deserción de Clientes
+# Empresa: QWE INC.
+
+library(readxl)
+library(tidyverse)
+library(ggplot2)
+library(flextable)
+library(officer)
+library(broom)
+library(caret)
+library(scales)
+
